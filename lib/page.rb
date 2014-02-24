59afd47c539eec5dc1ad630fda293b5bed734d51
Blake Erickson <o.blakeerickson@gmail.com>
2014-02-24 11:04:13 -0700

    add rake task to populate users with streaks greater than 365

---
+++ lib/page.rb
@@ -0,0 +1,43 @@
+class Page
+  def initialize(username)
+    page = page username
+    chunk = chunk(page)
+    if chunk != nil
+      @streak = get_streak(chunk)
+    else
+      @streak = 0
+    end
+  end
+
+  def streak
+    @streak
+  end
+
+  private
+
+  def page(username)
+    begin
+      open("https://github.com/#{username}").read
+    rescue
+      "error"
+    end
+  end
+
+  def chunk(page)
+    location = page.index '<div class="col contrib-streak">'
+    if location != nil
+      page[location..location+100]
+    end
+  end
+  def get_streak(chunk)
+    location = chunk.index 'days'
+    @streak = chunk[location-4..location-2]
+    if @streak[0,1] == '"'
+      @streak[0] = ''
+    end
+    if @streak[0,1] == '>'
+      @streak[0] = ''
+    end
+    @streak
+  end
+end
---
+++ lib/tasks/large_streaks.rake
@@ -0,0 +1,11 @@
+namespace :large_streaks do
+  desc "Fetch Longest Streak for Github Users with streaks greater than 365"
+  task populate: :environment do
+    rate_limit = 5000
+    connection = Connection.new
+    User.where('longest_streak > 365').find_each(batch_size: 10) do |user|
+      page = Page.new(user.username)
+      user.update_attribute(:longest_streak, page.streak)
+    end
+  end
+end
