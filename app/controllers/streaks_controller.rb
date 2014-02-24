4853d74d5f3e6f6d6ecf303ce615fdd5a9b5d36a
Blake Erickson <o.blakeerickson@gmail.com>
2014-02-24 11:57:40 -0700

    change top 20 to display top 50

--- app/controllers/streaks_controller.rb
+++ app/controllers/streaks_controller.rb
@@ -1,6 +1,6 @@
 class StreaksController < ApplicationController
   def index
-    @users = User.where.not(longest_streak:nil).limit(20).order(longest_streak: :desc)
+    @users = User.where.not(longest_streak:nil).limit(50).order(longest_streak: :desc)
     @count = Rails.cache.fetch("count", :expires_in => 4.hours) do
       User.count
     end
